8d6cb46ff96f40608f6f02183997b175dcb9ee0c
Muntazir999 <150386702+Muntazir999@users.noreply.github.com>
2023-12-16 21:56:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Enter the first number: "
+    },
+    {
+        type: "number",
+        name: "num2",
+        message: "Enter the second number: "
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Select the operator: ",
+        choices: ["Add", "Subtract", "Multiply", "Divide"]
+    }
+]);
+let result;
+switch (answer.operator) {
+    case "Add":
+        result = answer.num1 + answer.num2;
+        console.log("The answer for addition:" + result);
+        break;
+    case "Subtract":
+        result = answer.num1 - answer.num2;
+        console.log("The answer for subtraction:" + result);
+        break;
+    case "Multiply":
+        result = answer.num1 * answer.num2;
+        console.log("The answer for multiplition:" + result);
+        break;
+    case "Divide":
+        result = answer.num1 / answer.num2;
+        console.log("The answer for division:" + result);
+        break;
+}
